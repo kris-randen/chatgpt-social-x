c4a91d199152a476fdb3ca7ccd40e23ed2008aad
kris-randen <kris.randen@gmail.com>
2023-08-29 03:27:27 +0530

    Initial commit after creating the project socialX.

---
+++ socialX/socialX/socialXApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialXApp.swift
+//  socialX
+//
+//  Created by Krishnaswami Rajendren on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct socialXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
